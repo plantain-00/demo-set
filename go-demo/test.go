dde6e3406e623404468028fcf205b0ac3d95f2df
york yao <yaoao12306@outlook.com>
2016-02-03 18:15:22 +0800

    save

--- go-demo/test.go
+++ go-demo/test.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+var sum = 0
 
 func main() {
+	fmt.Println(math.Abs(-1))
 	fmt.Println("Hello, world")
+	fmt.Println(add(1, 2))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
