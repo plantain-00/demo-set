d8df80792a5a5150a7c3767b73e5bc16245501ac
york yao <yaoao12306@outlook.com>
2016-02-05 16:04:35 +0800

    save

--- go-demo/gin-demo/gin.go
+++ go-demo/gin-demo/gin.go
@@ -9,7 +9,10 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("books", func(context *gin.Context) {
-		context.String(http.StatusOK, "Hello world")
+		context.JSON(http.StatusOK, gin.H{
+			"message":   "Hello world",
+			"isSuccess": true,
+		})
 	})
 	router.Run(":8080")
 }
