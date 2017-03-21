abe1f56b63fd5c39a6549f615e23cc55cada74fc
york yao <yaoao12306@outlook.com>
2017-03-21 20:54:24 +0800

    add make file

--- c-demo/openssl/a.cpp
+++ c-demo/openssl/a.cpp
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <openssl/md5.h>
 
 int main()
 {
     unsigned char digest[MD5_DIGEST_LENGTH];
-    char string[] = "happy";
-    MD5((unsigned char *)&string, strlen(string), (unsigned char *)&digest);
-    char mdString[33];
-    for (int i = 0; i < 16; i++)
+    std::string raw("happy");
+    MD5((unsigned char *)(raw.c_str()), raw.length(), (unsigned char *)&digest);
+    char result[33];
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++)
     {
-        sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
+        sprintf(&result[i * 2], "%02x", (unsigned int)digest[i]);
     }
-    printf("md5 digest: %s\n", mdString);
+    printf("md5 digest: %s\n", result);
     return 0;
 }
